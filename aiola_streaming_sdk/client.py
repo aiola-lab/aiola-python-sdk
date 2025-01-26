6b8e24df29340e5c9506e97eda8f017ecd203a68
dorb-aiola <155890345+dorb-aiola@users.noreply.github.com>
2025-01-26 17:52:06 +0200

    Revert "Added '/' as a namespace"

--- aiola_streaming_sdk/client.py
+++ aiola_streaming_sdk/client.py
@@ -151,17 +151,12 @@ class AiolaStreamingClient:
 
             _transports = ['polling'] if self.config.transports == 'polling' else ['polling', 'websocket'] if self.config.transports == 'websocket' else ['polling', 'websocket']
             
-            namespaces = [self.config.namespace]
-            if self.config.namespace != '/':
-                namespaces.append('/')
-
             # Connect to the server
             self.sio.connect(
                 url=url,
                 transports=_transports,
                 headers=auth_headers.headers,
-                socketio_path='/api/voice-streaming/socket.io',
-                namespaces=namespaces
+                socketio_path='/api/voice-streaming/socket.io'
             )
 
             # Start streaming or wait
